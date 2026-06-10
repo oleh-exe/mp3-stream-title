40f758fc29ede9910d25aca0cb2c7bea838172fe
Oleh Kovalenko <oleh.exe.dev@gmail.com>
2026-06-11 00:30:48 +0300

    Add `StreamContextFactory` for creating HTTP stream contexts with request customization

---
+++ src/Infrastructure/Http/Request/StreamContextFactory.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright 2026 Oleh Kovalenko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace Mp3StreamTitle\Infrastructure\Http\Request;
+
+use InvalidArgumentException;
+use Mp3StreamTitle\Infrastructure\Http\HttpHeadersSerializer;
+
+final readonly class StreamContextFactory
+{
+    private HttpHeadersSerializer $headersSerializer;
+
+    public function __construct(
+        private HttpRequest $request,
+        private int $timeout,
+    ) {
+        $this->headersSerializer = new HttpHeadersSerializer();
+
+        if ($timeout <= 0) {
+            throw new InvalidArgumentException(
+                'Timeout must be greater than 0 seconds'
+            );
+        }
+    }
+
+    public function create()
+    {
+        return stream_context_create([
+            'http' => [
+                'method' => $this->request->method()->value,
+                'timeout' => $this->timeout,
+                'header' => $this->headersSerializer->toString($this->request->headers()),
+            ],
+        ]);
+    }
+}
