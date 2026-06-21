d0359757c64c9a0651e6548c8d6e2212ec93701c
Oleh Kovalenko <oleh.exe.dev@gmail.com>
2026-06-21 17:38:19 +0300

    Add `IcyHeaderHandler` for buffering HTTP headers and resolving ICY metadata offset logic

---
+++ src/Infrastructure/Http/IcyHeaderHandler.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright 2026 Oleh Kovalenko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace Mp3StreamTitle\Infrastructure\Http;
+
+final class IcyHeaderHandler
+{
+    public function __construct(
+        private readonly HttpHeaderBuffer $httpHeaderBuffer,
+        private readonly MetaIntResolver $metaIntResolver,
+        private readonly IcyMetadataStreamParser $parser,
+    ) {
+    }
+
+    public function handle(string $header): bool
+    {
+        $isComplete = $this->httpHeaderBuffer->append($header);
+
+        if ($isComplete) {
+            $metaInt = $this->metaIntResolver->resolve();
+
+            $this->parser->setOffset($metaInt);
+        }
+
+        return !$isComplete;
+    }
+}
