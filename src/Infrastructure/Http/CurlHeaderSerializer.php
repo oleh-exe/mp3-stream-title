2c77c233ed5e6cba1f573fb012819bb39adf2ecd
Oleh Kovalenko <oleh.exe.dev@gmail.com>
2026-06-23 01:00:19 +0300

    Introduce `CurlHeaderSerializer` to serialize HTTP headers in `Mp3StreamTitle` infrastructure.

---
+++ src/Infrastructure/Http/CurlHeaderSerializer.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright 2026 Oleh Kovalenko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace Mp3StreamTitle\Infrastructure\Http;
+
+use Mp3StreamTitle\Infrastructure\Http\Request\HeaderCollection;
+
+final class CurlHeaderSerializer
+{
+    /**
+     * @return list<string>
+     */
+    public function serialize(HeaderCollection $headers): array
+    {
+        /** @var list<string> $result */
+        $result = [];
+
+        foreach ($headers->all() as $name => $value) {
+            $result[] = sprintf('%s: %s', $name, $value);
+        }
+
+        return $result;
+    }
+}
