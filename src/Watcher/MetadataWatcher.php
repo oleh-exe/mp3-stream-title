6de2ef4a064d8ea1e24f03e80b7005600714c0e7
Oleh Kovalenko <oleh.exe.dev@gmail.com>
2026-06-28 22:22:15 +0300

    Introduce `MetadataWatcher` to monitor and yield stream metadata changes in `Mp3StreamTitle`.

---
+++ src/Watcher/MetadataWatcher.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Copyright 2026 Oleh Kovalenko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace Mp3StreamTitle\Watcher;
+
+use Generator;
+use InvalidArgumentException;
+use Mp3StreamTitle\Mp3StreamTitle;
+
+final class MetadataWatcher
+{
+    /**
+     * @param Mp3StreamTitle $client
+     * @param int $interval
+     */
+    public function __construct(
+        private readonly Mp3StreamTitle $client,
+        private readonly int $interval
+    ) {
+        if ($interval < 1) {
+            throw new InvalidArgumentException(
+                'Interval must be greater than zero.'
+            );
+        }
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return Generator<int, string>
+     */
+    public function watch(string $url): Generator
+    {
+        $lastTitle = null; // ?string
+
+        while (true) {
+            $title = $this->client->streamTitle($url);
+
+            if ($title !== $lastTitle) {
+                $lastTitle = $title;
+
+                yield $title;
+            }
+
+            sleep($this->interval);
+        }
+    }
+}
