2a16c682aa5858eb99fc1b84e94a380b9eb09917
Oleh Kovalenko <oleh.exe.dev@gmail.com>
2026-07-01 23:32:53 +0300

    Refactor `SocketConnection` to simplify formatting in conditional logic

--- src/Transport/Socket/SocketConnection.php
+++ src/Transport/Socket/SocketConnection.php
@@ -74,8 +74,7 @@ final class SocketConnection
             );
         }
 
-        if (
-            !in_array($this->state, [ConnectionState::INITIAL, ConnectionState::CLOSED], true)
+        if (!in_array($this->state, [ConnectionState::INITIAL, ConnectionState::CLOSED], true)
         ) {
             throw new LogicException(
                 sprintf(
