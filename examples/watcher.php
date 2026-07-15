ed9ac80e29c783c4b36a31c30aa8158672c54d8d
Oleh Kovalenko <oleh.exe.dev@gmail.com>
2026-07-15 22:25:19 +0300

    Add clarification for `interval` parameter in `MetadataWatcher` example

--- examples/watcher.php
+++ examples/watcher.php
@@ -19,7 +19,7 @@ use Mp3StreamTitle\Watcher\MetadataWatcher;
 
 $watcher = new MetadataWatcher(
     client: new Mp3StreamTitle(),
-    interval: 10,
+    interval: 10, // In seconds
 );
 
 $streamUrl = 'https://cast1.torontocast.com:4450/stream/1/'; // Blues Never Die
